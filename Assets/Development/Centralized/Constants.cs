40da40e5e26486dc9c192315d7a7bb31ac0437ae
Dumtard <charles.black90@gmail.com>
2018-05-04 18:54:18 -0400

    Add Assets directory

---
+++ Assets/Development/Centralized/Constants.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Constants : MonoBehaviour {
+
+    public enum GameStateNames
+    {
+        GS_00_NULL = -1,
+        GS_01_MENU = 0,
+        GS_02_LOADING = 1,
+        GS_03_INPLAY,
+        GS_04_LEAVING,
+        GS_05_MENURELOAD
+    }
+
+}
