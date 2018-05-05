0c576ebe90aacb56ca0760851c28a1eb8453de9c
Dumtard <charles.black90@gmail.com>
2018-05-05 13:36:23 -0400

    Random potholes and cleanup

--- Assets/Development/Managers/GameStateManager.cs
+++ Assets/Development/Managers/GameStateManager.cs
@@ -34,9 +34,6 @@ public class GameStateManager : MonoBehaviour {
 			m_currentGameStateIndex = m_nextGameStateIndex;
 			m_nextGameStateIndex = Enums.GameStateNames.GS_00_NULL;
 		}
-
-        Camera camera = Camera.main;
-        camera.transform.Translate(new Vector3(0.01f, 0, 0));
 	}
 
 	public void Init() {
--- Assets/Development/StageManager.cs
+++ Assets/Development/StageManager.cs
@@ -3,10 +3,20 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class StageManager : MonoBehaviour {
-    private Texture2D tex;
-    private Sprite sprite;
+    private Texture2D pothole1Texture;
+    private Texture2D pothole2Texture;
+    private Texture2D groundTexture;
+
+    private Sprite pothole2Sprite;
+    private Sprite pothole1Sprite;
+    private Sprite groundSprite;
+
+    private Camera camera;
+
+    public float potholeChance = 0.2f;
 
     private List<GameObject> backgroundObjects = new List<GameObject>();
+    private List<GameObject> potholes = new List<GameObject>();
 
     Vector3 PixelToGame(float x, float y, float z) {
         Vector3 result = new Vector3(x / 100, y / 100, z / 100);
@@ -19,47 +29,86 @@ public class StageManager : MonoBehaviour {
         return result;
     }
 
+    bool isOutOfBounds(GameObject obj) {
+        float cameraLeftEdge = GameToPixel(camera.transform.position.x, 0, 0).x - camera.rect.width;
+        float objRightEdge = GameToPixel(obj.transform.position.x, 0, 0).x + obj.GetComponent<SpriteRenderer>().sprite.rect.width;
+
+        if (cameraLeftEdge > objRightEdge)
+        {
+            return true;
+        }
+        return false;
+    }
+
+    void addNewTile() {
+        GameObject obj = new GameObject();
+
+        if (backgroundObjects.Count == 0) {
+            obj.transform.SetPositionAndRotation(PixelToGame(camera.transform.position.x - camera.rect.width * 2, 0, 0), obj.transform.rotation);
+        } else {
+            GameObject lastTile = backgroundObjects[backgroundObjects.Count - 1];
+            obj.transform.SetPositionAndRotation(PixelToGame(GameToPixel(lastTile.transform.position.x, 0, 0).x + groundTexture.width, 0, 0), obj.transform.rotation);
+        }
+
+        SpriteRenderer rend = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+        rend.sprite = groundSprite;
+
+        backgroundObjects.Add(obj);
+
+        if (Random.Range(0.0f, 1.0f) < potholeChance)
+        {
+            float potholePosition = Random.Range(0.0f, 5.0f);
+            GameObject pothole = new GameObject();
+            SpriteRenderer renderer = pothole.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
+
+            Vector3 pos = GameToPixel(obj.transform.position.x, obj.transform.position.y, 0);
+            pothole.transform.SetPositionAndRotation(PixelToGame(pos.x + Random.Range(0.0f, groundSprite.rect.width - pothole2Sprite.rect.width), pos.y + Random.Range(-100.0f, 80.0f), -0.01f), pothole.transform.rotation);
+
+            renderer.sprite = pothole2Sprite;
+            potholes.Add(pothole);
+        }
+    }
+
     // Use this for initialization
     void Start () {
-        Camera camera = Camera.main;
+        camera = Camera.main;
+        
+        pothole2Texture = Resources.Load("background/bg_0000_pothole2") as Texture2D;
+        pothole1Texture = Resources.Load("background/bg_0001_pothole1") as Texture2D;
+        groundTexture = Resources.Load("background/bg_0002_ground") as Texture2D;
 
-        //sprite = Resources.Load("Tower", typeof(Sprite)) as Sprite;
-        tex = Resources.Load("background/bg_0002_ground") as Texture2D;
-
-        sprite = Sprite.Create(tex, new Rect(0, 0, tex.width, tex.height), new Vector2(0f, 0.5f), 100);
+        pothole2Sprite = Sprite.Create(pothole2Texture, new Rect(0, 0, pothole2Texture.width, pothole2Texture.height), new Vector2(0f, 0.5f), 100);
+        pothole1Sprite = Sprite.Create(pothole1Texture, new Rect(0, 0, pothole1Texture.width, pothole1Texture.height), new Vector2(0f, 0.5f), 100);
+        groundSprite = Sprite.Create(groundTexture, new Rect(0, 0, groundTexture.width, groundTexture.height), new Vector2(0f, 0.5f), 100);
 
         for (int i = 0; i < 3; i++) {
-            GameObject obj = new GameObject();
-            //Attach a SpriteRenender to the newly created gameobject
-            SpriteRenderer rend = obj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
-            obj.transform.SetPositionAndRotation(PixelToGame(camera.transform.position.x - camera.rect.width + (i * tex.width), 0, 0), obj.transform.rotation);
-
-            rend.sprite = sprite;
-
-            backgroundObjects.Add(obj);
+            addNewTile();
         }
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (backgroundObjects.Count > 0) {
-            Camera camera = Camera.main;
 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (backgroundObjects.Count > 0)
+        {
             GameObject obj = backgroundObjects[0];
-            if (GameToPixel(camera.transform.position.x, 0, 0).x - camera.rect.width > GameToPixel(obj.transform.position.x, 0, 0).x + obj.GetComponent<SpriteRenderer>().sprite.rect.width) {
-                
-                GameObject newObj = new GameObject();
-                //Attach a SpriteRenender to the newly created gameobject
-                SpriteRenderer rend = newObj.AddComponent(typeof(SpriteRenderer)) as SpriteRenderer;
-                newObj.transform.SetPositionAndRotation(PixelToGame(GameToPixel(obj.transform.position.x, 0, 0).x + (3 * obj.GetComponent<SpriteRenderer>().sprite.rect.width), 0, 0), newObj.transform.rotation);
-
-                rend.sprite = sprite;
-
-                backgroundObjects.Add(newObj);
+            if (isOutOfBounds(obj))
+            {
+                addNewTile();
 
                 GameObject.Destroy(obj);
                 backgroundObjects.Remove(obj);
             }
         }
-	}
+        if (potholes.Count > 0)
+        {
+            GameObject obj = potholes[0];
+            if (isOutOfBounds(obj))
+            {
+                GameObject.Destroy(obj);
+                potholes.Remove(obj);
+            }
+        }
+    }
 }
