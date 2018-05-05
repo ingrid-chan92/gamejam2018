9a52651a9ec214c5b97ae095aef5de2e34aa2794
John Allison <github@allsn.ca>
2018-05-05 13:46:57 -0400

    Player stuff start

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed = 10f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float step = speed * Time.deltaTime;
+
+		if (Input.GetKey(KeyCode.A))
+        {
+            transform.position = transform.position + step * Vector3.left;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position = transform.position + step * Vector3.right;
+        }
+    }
+}
