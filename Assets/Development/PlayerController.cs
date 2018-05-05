52e38b75809d3b9bafead2618e3bbf9fac9d93c8
John Allison <github@allsn.ca>
2018-05-05 14:12:27 -0400

    Player moves up and down

--- Assets/Development/PlayerController.cs
+++ Assets/Development/PlayerController.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-    public float speed = 10f;
+    public float speed = 2f;
+    public float ySpeedMult = 0.5f; // Multiplier for speed when moving up or down
     private Camera camera;
 
 	// Use this for initialization
@@ -19,15 +20,25 @@ public class PlayerController : MonoBehaviour {
 		if (Input.GetKey(KeyCode.A))
         {
             transform.position = transform.position + step * Vector3.left;
+            transform.rotation = Quaternion.identity;
         }
         if (Input.GetKey(KeyCode.D))
         {
             transform.position = transform.position + step * Vector3.right;
+            transform.rotation = Quaternion.AngleAxis(180, Vector3.up);
 
-            if(transform.position.x > camera.transform.position.x)
+            if (transform.position.x > camera.transform.position.x)
             {
                 camera.transform.Translate(step * Vector3.right);
             }
+        }
+        if (Input.GetKey(KeyCode.W))
+        {
+            transform.position = transform.position + step * Vector3.up * ySpeedMult;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            transform.position = transform.position + step * Vector3.down * ySpeedMult;
         }
     }
 }
